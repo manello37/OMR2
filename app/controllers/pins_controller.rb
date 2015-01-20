186e29585ed4e209b0f3c82b01db234a3d7bb863
Mike Anello <manello37@yahoo.com>
2015-01-20 02:46:32 -0500

    Ordering Pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -5,9 +5,9 @@ class PinsController < ApplicationController
 
   respond_to :html
 
- def index
-    @pins = Pin.all
-  end
+  def index
+   @pins = Pin.all.order("created_at DESC")
+ end
 
   def show
   end
